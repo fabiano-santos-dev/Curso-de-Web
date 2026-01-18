4eab3f1d69bd1bbe052e2ea61e854418cd69597b
Fabiano <fabiano.alves.dev@gmail.com>
2026-01-18 13:46:18 -0300

    Aprendendo sobre Comentários

---
+++ FUNDAMENTOS/_02_comentarios.js
@@ -0,0 +1,16 @@
+// Comentários são textos que você descreve sobre o código para explicar alguma informação.
+// Os comentários podem ser feitos de duas formas:
+
+// Como comentário de uma linha, como essa.
+
+
+/**
+   E comentário de várias linhas 
+   como  essas linhas dentro desse bloco.   
+ */
+
+// O que identifica cada um são suas iniciais, o comentário de uma linha é representado por //
+// E comentário de várias linhas é representado por /* e */
+
+// Outro ponto, comentários não são interpretado pelo programa.Ou seja não geram erros no programa.
+// São como se não estivessem ali.
--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Curso de Desenvolvimento Web - Html, Css e JavaScript
 
 # Fundamentos
 [ 1° - Sobre Sentença de Código](https://github.com/fabiano-santos-dev/Curso-de-Web/blob/main/FUNDAMENTOS/_01_sentenca_de_codigo.js)
+[ 2° - Sobre Comentários](https://github.com/fabiano-santos-dev/Curso-de-Web/blob/main/FUNDAMENTOS/_02_comentarios.js)
+[ 3° - Sobre Bloco de Código](https://github.com/fabiano-santos-dev/Curso-de-Web/blob/main/FUNDAMENTOS/_03_bloco_de_codigo.js)
