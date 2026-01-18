e16dff911355ae531f1a158cf333c455cebd612b
Fabiano <fabiano.alves.dev@gmail.com>
2026-01-18 12:43:59 -0300

    Iniciando curso de web, Hoje é dia 18 de Janeiro de 2026

---
+++ FUNDAMENTOS/_01_sentenca_de_codigo.js
@@ -0,0 +1,15 @@
+/* Uma sentença de código é uma instrução que termina com um ponto e virgula.
+   Ou seja uma sentença de código é um comando que você está passando para o 
+   computador fazer alguma coisa.
+   Exemplo - Abaixo:
+*/
+console.log("Toda essa linha é considerada uma sentença de código");
+
+/**
+  Outro ponto as instruções são lidas de cima para baixo.
+  Como as instruções abaixo.
+ */
+
+console.log("Primeira istrução lida pelo computador.");
+console.log("Segunda istrução lida pelo computador.");
+console.log("Terceira istrução lida pelo computador.");
