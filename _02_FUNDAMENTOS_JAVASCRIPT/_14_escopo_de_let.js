ac21698acb99b1a1f599bfb5be4d33e256ad7e6c
Fabiano <fabiano.alves.dev@gmail.com>
2026-01-28 20:36:02 -0300

    Como funciona o loop com var e com let

--- _02_FUNDAMENTOS_JAVASCRIPT/_14_escopo_de_let.js
+++ _02_FUNDAMENTOS_JAVASCRIPT/_14_escopo_de_let.js
@@ -2,6 +2,6 @@ let numero = 1;
 
 {
     let numero = 2; // Procura o escopo menor
-    console.log(numero);
+    console.log("Dentro = ",numero);
 }
-console.log(numero);
+console.log("Fora =",numero);
---
+++ _02_FUNDAMENTOS_JAVASCRIPT/_15_usando_var_em_loop.js
@@ -0,0 +1,19 @@
+for(var i = 0;i <= 10;i++){
+    console.log(i);
+}
+
+console.log(i);
+
+console.log();
+/*
+    Como var não é uma função,i tem escopo global
+    portanto quando a condição do loop e satisfeita i ainda é incrementada mais uma vez
+    imprimindo 11
+*/
+
+for(let j = 0;j <= 10;j++){
+    console.log(j);
+}
+// Aqui vai gerar um erro, porque vai entender que j não foi declarada.
+// o que significa que a variável j só funciona dentro do for.
+//console.log(j)
