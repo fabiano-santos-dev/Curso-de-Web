66692cc52e044820df19b3dbd14d7090794a5be5
Fabiano <fabiano.alves.dev@gmail.com>
2026-01-30 21:46:55 -0300

    reorganizando conceito de hosting

--- _02_FUNDAMENTOS_JAVASCRIPT/_16_conceito_de_hosting.js
+++ _02_FUNDAMENTOS_JAVASCRIPT/_16_conceito_de_hosting.js
@@ -3,4 +3,4 @@ console.log(valor);
 var valor = 66;
 console.log(valor);
 
-// Conceito de hosting tem haver com subir o valor da variavel pra cima.
+// Conceito de hosting tem haver com a elevação valor da variavel.
